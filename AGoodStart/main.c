16d9682bef1089d257800326671e48a1a1ce41fc
GRADY <gjenkins205@gmail.com>
2017-12-08 19:51:10 -0500

    Initial Commit

---
+++ AGoodStart/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Grady Jenkins on 12/8/17.
+//  Copyright © 2017 Grady. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
