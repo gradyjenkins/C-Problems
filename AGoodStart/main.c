fca00ce896e0e810665c986e82b32dc9662bc4ac
GRADY <gjenkins205@gmail.com>
2017-12-10 20:54:53 -0500

    First C problem

--- AGoodStart/main.c
+++ AGoodStart/main.c
@@ -7,9 +7,20 @@
 //
 
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int remainingAngle(int side1, int side2)
+{
+    int result = (180 - (side1 + side2));
+    return result;
+}
+
+int main(int argc, const char * argv[])
+{
+    float angle1 = 30.0;
+    float angle2 = 60.0;
+    float angle3 = remainingAngle(angle1, angle2);
+    printf("The third angle is %.2f\n", angle3);
+    return EXIT_SUCCESS;
 }
